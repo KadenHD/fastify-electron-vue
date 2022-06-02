960c087cfd1674438150682166819ae334ffedb2
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-02 11:15:24 +0200

    fix errors

--- electron_vue-client/src/App.vue
+++ electron_vue-client/src/App.vue
@@ -2,55 +2,44 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <router-link class="nav-link" :to="{ name: 'Home' }">Home</router-link>
+        <router-link class="nav-link ml-2" :to="{ name: 'About' }"
+          >About</router-link
+        >
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://vuetifyjs.com/en/getting-started/installation/"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        Vuetify doc
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
-
+<script lang="ts">
 export default {
   name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+.nav-link {
+  text-decoration: none;
+  color: white !important;
+}
+</style>
