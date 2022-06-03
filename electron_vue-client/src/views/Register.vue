b9d7b4d4c7a756e262e77f5568380c430cc6993e
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-03 16:29:16 +0200

    Update Register.vue

--- electron_vue-client/src/views/Register.vue
+++ electron_vue-client/src/views/Register.vue
@@ -1,24 +1,89 @@
 <template>
   <div class="Register">
     <card-wrapper :title="title" :subtitle="subtitle">
-      <ui-input />
-      <ui-button />
+      <v-form ref="form">
+        <ui-input
+          v-for="(item, index) in inputs"
+          :key="index"
+          :rules="item.rules"
+          :label="item.label"
+          :counter="item.counter"
+          :type="item.type"
+          v-model="item.value"
+        />
+        <ui-button
+          :handleClick="submitRegisterForm"
+          color="primary"
+          value="S'incrire"
+        />
+      </v-form>
     </card-wrapper>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
 import CardWrapper from "@/components/CardWrapper.vue";
 import UiInput from "@/components/ui/Input.vue";
 import UiButton from "@/components/ui/Button.vue";
+import {
+  firstNameRules,
+  lastNameRules,
+  emailRules,
+  passwordRules,
+} from "@/plugins/inputRules";
 
-export default {
-  data() {
-    const title: string = "Inscription";
-    const subtitle: string =
-      "Nous ne montrons vos informations personnelles à aucun autre utilisateur";
-    return { title, subtitle };
-  },
+export default Vue.extend({
   components: { CardWrapper, UiInput, UiButton },
-};
+  data: () => ({
+    inputs: {
+      lastname: {
+        value: null,
+        rules: lastNameRules,
+        label: "Nom",
+        counter: false,
+        type: "text",
+      },
+      firstname: {
+        value: null,
+        rules: firstNameRules,
+        label: "Prénom",
+        counter: false,
+        type: "text",
+      },
+      email: {
+        value: null,
+        rules: emailRules,
+        label: "E-mail",
+        counter: false,
+        type: "email",
+      },
+      password: {
+        value: null,
+        rules: passwordRules,
+        label: "Mot de passe",
+        counter: true,
+        type: "password",
+      },
+    },
+    valid: false,
+    title: "Inscription",
+    subtitle:
+      "Nous ne montrons vos informations personnelles à aucun autre utilisateur",
+  }),
+  methods: {
+    submitRegisterForm() {
+      const form: any = this.$refs.form;
+      if (form.validate()) {
+        const data = {
+          lastname: this.inputs.lastname.value,
+          firstname: this.inputs.firstname.value,
+          email: this.inputs.email.value,
+          password: this.inputs.password.value,
+        };
+        console.log(data);
+      }
+    },
+  },
+});
 </script>
