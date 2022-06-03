781bf63fb9e6d614bd7a61edacff386ce1c92800
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-03 16:29:10 +0200

    Update Input.vue

--- electron_vue-client/src/components/ui/Input.vue
+++ electron_vue-client/src/components/ui/Input.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="UiInput"></div>
+  <div class="UiInput">
+    <v-text-field
+      :rules="rules"
+      :label="label"
+      :counter="counter"
+      :type="type"
+      :value="value"
+      @input="$emit('input', $event)"
+    ></v-text-field>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "UiInput",
+  props: {
+    rules: Array,
+    label: String,
+    counter: Boolean,
+    type: String,
+    value: String,
+  },
 };
 </script>
