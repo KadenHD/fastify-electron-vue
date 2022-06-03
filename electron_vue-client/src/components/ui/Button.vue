57b016afc9362f43229f7275c53cf2a87703771c
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-03 16:29:07 +0200

    Update Button.vue

--- electron_vue-client/src/components/ui/Button.vue
+++ electron_vue-client/src/components/ui/Button.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="UiButton"></div>
+  <div class="UiButton">
+    <v-btn class="mt-4" @click="handleClick" :color="color">
+      {{ value }}
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "UiButton",
+  props: {
+    handleClick: Function,
+    color: String,
+    value: String,
+  },
 };
 </script>
