0c345b9b11d2dd88285b8860f820340a4a487a79
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-03 16:29:04 +0200

    Update CardWrapper.vue

--- electron_vue-client/src/components/CardWrapper.vue
+++ electron_vue-client/src/components/CardWrapper.vue
@@ -2,9 +2,11 @@
   <div class="Wrapper">
     <v-row class="ml-1 mr-1 mt-4" justify="center">
       <v-card width="800" elevation="8">
-        <v-card-title v-if="title">{{ title }}</v-card-title>
-        <slot></slot>
-        <v-card-subtitle v-if="subtitle">{{ subtitle }}</v-card-subtitle>
+        <v-container>
+          <v-card-title v-if="title">{{ title }}</v-card-title>
+          <slot></slot>
+          <v-card-subtitle v-if="subtitle">* {{ subtitle }}</v-card-subtitle>
+        </v-container>
       </v-card>
     </v-row>
   </div>
