b2bfe405d2d5091e9f2c862d12114d9a1086a7d2
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-07 11:44:10 +0200

    Update Home.vue

--- electron_vue-client/src/views/Home.vue
+++ electron_vue-client/src/views/Home.vue
@@ -1,3 +1,14 @@
 <template>
   <div class="Home">Accueil</div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Home",
+  components: {},
+});
+</script>
+
+<style lang="scss">
+</style>
