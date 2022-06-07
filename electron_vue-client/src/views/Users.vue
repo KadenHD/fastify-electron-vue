09089631f3447dc0671e21d7c7e6a0836f19e944
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-07 11:44:17 +0200

    Update Users.vue

--- electron_vue-client/src/views/Users.vue
+++ electron_vue-client/src/views/Users.vue
@@ -1,3 +1,14 @@
 <template>
   <div class="Users">Utilisateurs</div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Users",
+  components: {},
+});
+</script>
+
+<style lang="scss">
+</style>
