bc8682fa964bf825cfb48e29a0837c1088418d02
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-07 15:42:57 +0200

    Update inputRules.ts

--- electron_vue-client/src/plugins/inputRules.ts
+++ electron_vue-client/src/plugins/inputRules.ts
@@ -1,25 +1,31 @@
+
+const regex_email = () => { return /.+@.+\..+/; };
+const regex_password = () => { return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.*\d).*$/; }; /* at least one Uppercase, one Lowercase, one special char and one number in any order.*/
+const regex_name = () => { return /^(?=^[a-zA-Z-]*$)(?=.*[A-Z]+[a-z][A-Z]*)/; }; /* Start with one Uppercase, one Lowercase just after, white space, special char and number not allowed only hyphen.*/
+const regex_min = (a: String) => { return new RegExp("^.{" + a + ",9999}$", "g"); };
+const regex_max = (a: String) => { return new RegExp("^.{0," + a + "}$", "g"); };
+
 export const emailRules = [
     (v: string) => !!v || "L'e-mail est requis",
-    (v: string) => /.+@.+\..+/.test(v) || "Le format de l'e-mail doit être valide",
-    (v: string) => v && v.length <= 100 || "L'e-mail doit faire maximum 100 caractères",
+    (v: string) => regex_email().test(v) || "Le format de l'e-mail doit être valide",
+    (v: string) => regex_max("100").test(v) || "L'e-mail doit faire maximum 100 caractères",
 ]
 
 export const passwordRules = [
     (v: string) => !!v || "Le mot de passe est requis",
-    (v: string) => (/[A-Z]/.test(v) && /[a-z]/.test(v) && /[0-9]/.test(v) && /[^A-Za-z0-9]/.test(v)) ||
-        "Le mot de passe doit contenir : une majuscule, une minuscule, un chiffre, un caractère spécial",
-    (v: string) => v && v.length > 7 || "Le mot de passe doit faire minimum 8 caractères",
-    (v: string) => v && v.length <= 60 || "Le mot de passe doit faire maximum 60 caractères",
+    (v: string) => regex_password().test(v) || "Le mot de passe doit contenir : une majuscule, une minuscule, un chiffre, un caractère spécial",
+    (v: string) => regex_min("8").test(v) || "Le mot de passe doit faire minimum 8 caractères",
+    (v: string) => regex_max("60").test(v) || "Le mot de passe doit faire maximum 60 caractères",
 ]
 
 export const lastNameRules = [
     (v: string) => !!v || "Le nom est requis",
-    (v: string) => v && v.charAt(0) == v.charAt(0).toUpperCase() || "Le nom doit commencer par une majuscule",
-    (v: string) => v && v.length <= 50 || "Le nom doit faire maximum 50 caractères",
+    (v: string) => regex_name().test(v) || "Le nom doit commencer par une majuscule",
+    (v: string) => regex_max("50").test(v) || "Le nom doit faire maximum 50 caractères",
 ]
 
 export const firstNameRules = [
     (v: string) => !!v || "Le prénom est requis",
-    (v: string) => v && v.charAt(0) == v.charAt(0).toUpperCase() || "Le prénom doit commencer par une majuscule",
-    (v: string) => v && v.length <= 50 || "Le prénom doit faire maximum 50 caractères",
+    (v: string) => regex_name().test(v) || "Le prénom doit commencer par une majuscule",
+    (v: string) => regex_max("50").test(v) || "Le prénom doit faire maximum 50 caractères",
 ]
