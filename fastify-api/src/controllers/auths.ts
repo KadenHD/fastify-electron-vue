cdea28656ed0e7a860030e1bf73d0553c6ac28eb
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-08 15:25:40 +0200

    Update auths.ts

--- fastify-api/src/controllers/auths.ts
+++ fastify-api/src/controllers/auths.ts
@@ -1,7 +1,42 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
+import * as bcrypt from 'bcrypt'
+import { FastifyReply, FastifyRequest } from "fastify"
+import User from '../models/User'
+import { isNotAuth, isValidLoginForm, isValidRegisterForm } from "../validations/auths"
 
-export const login = async (server: FastifyInstance, req: FastifyRequest, res: FastifyReply) => {
-    const token = server.jwt.sign({ "username": "Adam Smith" })
-    console.log(token)
-    res.send({ token })
+export const login = async (req: FastifyRequest, res: FastifyReply) => {
+    if (isNotAuth(req.headers.authorization)) {
+        if (await isValidLoginForm(req.body)) {
+            const body: any = req.body
+            const data: any = await User.findOne({ where: { email: body.email } })
+            // créer le token de session et l'envoyer
+            res.send({ token: 'token', currentUser: data })
+        } else {
+            res.send('Le formulaire est invalide')
+        }
+    } else { res.send('Vous êtes connecté') }
+}
+
+export const register = async (req: FastifyRequest, res: FastifyReply) => {
+    if (isNotAuth(req.headers.authorization)) {
+        if (await isValidRegisterForm(req.body)) {
+            const body: any = req.body
+            const salt: any = bcrypt.genSaltSync(10)
+            const hashedPassword: any = bcrypt.hashSync(body.password, salt)
+            const data: any = {
+                lastname: body.lastname,
+                firstname: body.firstname,
+                email: body.email,
+                password: hashedPassword
+            }
+            await User.create(data)
+                .then(() => {
+                    res.send('Le compte a été créé avec succès')
+                })
+                .catch((err) => {
+                    res.send('Une erreur est survenue lors de la création du compte')
+                })
+        } else {
+            res.send('Le formulaire est invalide')
+        }
+    } else { res.send('Vous êtes connecté') }
 }
