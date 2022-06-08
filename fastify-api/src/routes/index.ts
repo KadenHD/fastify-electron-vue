83684e6a702a6f48b218d181c3843f20efe3f1d3
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-08 15:25:39 +0200

    Update index.ts

--- fastify-api/src/routes/index.ts
+++ fastify-api/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { FastifyInstance } from "fastify"
-import userRoutes from "./users"
 import authRoutes from "./auths"
 
 const router = (server: FastifyInstance, opts: any, done: () => void) => {
     server.register(authRoutes, { prefix: '/auths' })
-    server.register(userRoutes, { prefix: '/users' })
     done()
 }
 
