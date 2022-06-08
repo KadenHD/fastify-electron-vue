543cdb6990ea128424703c6146bf342d4fd0aa6d
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-08 15:25:42 +0200

    Update database.ts

--- fastify-api/src/configs/database.ts
+++ fastify-api/src/configs/database.ts
@@ -7,7 +7,7 @@ const database = new Sequelize(
     process.env.DB_USER as string,
     process.env.DB_PASS as string,
     {
-        timezone: '+01:00',
+        timezone: '+00:00',
         dialect: 'mysql',
         host: process.env.DB_HOST as string,
         logging: false /* Uncomment while on prod to avoid database logs (true by default)) */
