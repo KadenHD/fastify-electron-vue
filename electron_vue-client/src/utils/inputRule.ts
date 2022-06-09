7f674f7e21d64a2413021ffce54b30771a2bba18
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:48 +0200

    Update inputRule.ts

--- electron_vue-client/src/utils/inputRule.ts
+++ electron_vue-client/src/utils/inputRule.ts
@@ -8,23 +8,23 @@ export const emailRule = [
     (v: string) => !!v || "L'e-mail est requis",
     (v: string) => regex_email().test(v) || "Le format de l'e-mail doit être valide",
     (v: string) => regex_max("100").test(v) || "L'e-mail doit faire maximum 100 caractères",
-]
+];
 
 export const passwordRule = [
     (v: string) => !!v || "Le mot de passe est requis",
     (v: string) => regex_password().test(v) || "Le mot de passe doit contenir : une majuscule, une minuscule, un chiffre, un caractère spécial",
     (v: string) => regex_min("8").test(v) || "Le mot de passe doit faire minimum 8 caractères",
     (v: string) => regex_max("60").test(v) || "Le mot de passe doit faire maximum 60 caractères",
-]
+];
 
 export const lastNameRule = [
     (v: string) => !!v || "Le nom est requis",
     (v: string) => regex_name().test(v) || "Le nom doit commencer par une majuscule",
     (v: string) => regex_max("50").test(v) || "Le nom doit faire maximum 50 caractères",
-]
+];
 
 export const firstNameRule = [
     (v: string) => !!v || "Le prénom est requis",
     (v: string) => regex_name().test(v) || "Le prénom doit commencer par une majuscule",
     (v: string) => regex_max("50").test(v) || "Le prénom doit faire maximum 50 caractères",
-]
+];
