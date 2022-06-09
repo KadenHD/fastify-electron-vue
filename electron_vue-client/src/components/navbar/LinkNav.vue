a17398cb04eea909e6ec794ac7bc254a31913d48
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:30 +0200

    Create LinkNav.vue

---
+++ electron_vue-client/src/components/navbar/LinkNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="LinkNav">
+    <div class="ml-2 mr-2 nav-link" v-for="(item, i) in linkTab" :key="i">
+      <router-link :to="{ name: item.value }">{{ item.text }}</router-link>
+    </div>
+    <v-spacer></v-spacer>
+    <div class="ml-2 mr-2 nav-link" v-if="logout">
+      <a @click="clickedLogout()">Déconnexion</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "LinkNav",
+  props: {
+    linkTab: Array,
+    logout: Boolean,
+  },
+  methods: {
+    clickedLogout() {
+      console.log("logout");
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.LinkNav {
+  .nav-link a {
+    text-decoration: none;
+  }
+  .router-link-exact-active,
+  .router-link-active {
+    color: var(--v-primary-lighten2);
+  }
+}
+</style>
