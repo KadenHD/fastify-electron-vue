71a4ad04452f62854a209eee3080637d0efbe3c7
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:46 +0200

    Update beforeEnter.ts

--- electron_vue-client/src/utils/beforeEnter.ts
+++ electron_vue-client/src/utils/beforeEnter.ts
@@ -1,11 +1,18 @@
 import store from '@/store';
+import { getToken } from '@/utils/token';
 
-export const isNotLogged = (to, from, next) => {
-    if (store.getters.currentUser || localStorage.getItem('token_en_question')) next({ name: 'Home' });
-    else next();
+export const isNotLogged = (to: any, from: any, next: any) => {
+    if (store.getters.currentUser || getToken()) { next({ name: 'Home' }) }
+    else { next() }
 }
 
-export const isLogged = (to, from, next) => {
-    if (!store.getters.currentUser || !localStorage.getItem('token_en_question')) next({ name: 'Home' });
+export const isLogged = (to: any, from: any, next: any) => {
+    if (!store.getters.currentUser || !getToken()) { next({ name: 'Home' }) }
+    else { next() }
+}
+
+export const homeRedirections = (to: any, from: any, next: any) => {
+    if (store.getters.currentUser && store.getters.currentUser.role.label === 'ROLE_ADMIN') next({ name: 'Admin' });
+    else if (store.getters.currentUser && store.getters.currentUser.role.label === 'ROLE_USER') next({ name: 'User' });
     else next();
 }
