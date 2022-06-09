acab8ffa10e56d92104bd5c520195beb47cc4d50
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:43 +0200

    Update index.ts

--- electron_vue-client/src/store/index.ts
+++ electron_vue-client/src/store/index.ts
@@ -2,26 +2,38 @@ import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import authStore from '@/store/auth';
-import userStore from '@/store/user';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    alert: {
+      type: null,
+      message: null,
+    },
   },
   getters: {
-
+    alert: (state) => { return state.alert; },
   },
   actions: {
-
+    alertSuccess(context, alert) {
+      context.commit('alert', { type: 'success', message: alert });
+      setTimeout(() => {
+        context.commit('alert', {});
+      }, 4000)
+    },
+    alertError(context, alert) {
+      context.commit('alert', { type: 'error', message: alert });
+      setTimeout(() => {
+        context.commit('alert', {});
+      }, 4000)
+    },
   },
   mutations: {
-
+    alert(state, alert) { state.alert = alert; },
   },
   modules: {
     authStore,
-    userStore
   },
   plugins: [createPersistedState()]
 });
