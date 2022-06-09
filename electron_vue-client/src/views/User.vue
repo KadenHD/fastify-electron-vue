6c550fa13f6eecbacaa68829806a529730f223fd
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:59 +0200

    Create User.vue

---
+++ electron_vue-client/src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Home">User</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "User",
+  components: {},
+});
+</script>
+
+<style lang="scss">
+</style>
