fb07cfd2a7208bb83eddf8d620b9ef523299878d
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:39 +0200

    Update auth.ts

--- electron_vue-client/src/store/auth.ts
+++ electron_vue-client/src/store/auth.ts
@@ -1,17 +1,59 @@
-// import axios from 'axios';
-// import router from '@/router';
+import axios from 'axios';
+import router from '@/router';
+import { getToken, removeToken, setToken } from '@/utils/token';
+
+const prefix: string = '/auths/';
 
 export default {
     state: {
         currentUser: null,
     },
     getters: {
-        currentUser: (state) => { return state.currentUser; },
+        currentUser: (state: any) => { return state.currentUser; },
     },
     actions: {
-        currentUser(context, currentUser) { context.commit('currentUser', currentUser); },
+        currentUser(context: any, currentUser: any) { context.commit('currentUser', currentUser); },
+        setCurrentUser(context: any) {
+            if (getToken()) {
+                axios.get(prefix + "currentUser")
+                    .then((response) => {
+                        context.dispatch("currentUser", response.data.currentUser);
+                        if (!response.data.currentUser) {
+                            removeToken()
+                        }
+                    })
+                    .catch(() => {
+                        context.dispatch("logout");
+                    });
+            }
+        },
+        login(context: any, data: any) {
+            axios
+                .post(prefix + "login", data)
+                .then((response) => {
+                    setToken(response.data.token);
+                    context.dispatch("currentUser", response.data.currentUser);
+                    if (response.data.currentUser.role.label === "ROLE_ADMIN") {
+                        router.push({ name: "Admin" });
+                    } else if (response.data.currentUser.role.label === "ROLE_USER") {
+                        router.push({ name: "User" });
+                    }
+                })
+                .catch((error) => {
+                    console.log(error.response)
+                });
+        },
+        logout(context: any) {
+            context.commit('currentUser', null);
+            removeToken();
+            context.commit('clearAll');
+            router.push({ name: "Home" });
+        },
+        clearAll(context: any) {
+            context.commit("alert", {});
+        },
     },
     mutations: {
-        currentUser(state, currentUser) { state.currentUser = currentUser; },
+        currentUser(state: any, currentUser: any) { state.currentUser = currentUser; },
     },
-}
+};
