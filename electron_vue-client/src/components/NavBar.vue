c385f9930b64a1524bb9fd686db149d6c1332af1
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-06-09 12:15:26 +0200

    Update NavBar.vue

--- electron_vue-client/src/components/NavBar.vue
+++ electron_vue-client/src/components/NavBar.vue
@@ -1,62 +1,69 @@
 <template>
   <div class="NavBar">
-    <v-app-bar app absolute elevation="8">
-      <router-link class="mr-1 ml-1" :to="{ name: 'Default' }">
-        <v-img
-          src="@/assets/logo.png"
-          lazy-src="@/assets/logo.png"
-          class="mx-auto"
-          max-width="50"
-        ></v-img
-      ></router-link>
-      <v-divider class="mr-5 ml-2" vertical inset></v-divider>
-      <v-app-bar-title class="hidden-md-and-up" align-items="center">{{
-        appTitle
-      }}</v-app-bar-title>
-      <div class="hidden-sm-and-down">
-        <v-row justify="start" align-items="center">
-          <div class="ml-2 mr-2 nav-link" v-for="(item, i) in linkTab" :key="i">
-            <router-link :to="{ name: item.value }">{{
-              item.text
-            }}</router-link>
-          </div>
-        </v-row>
-      </div>
-      <v-spacer></v-spacer>
-      <v-app-bar-nav-icon
-        id="drawer"
-        class="hidden-md-and-up"
-        @click.stop="drawer = !drawer"
-      />
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title> Actions </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list nav dense>
-        <v-list-item-group>
-          <v-list-item
-            v-for="(item, i) in linkTab"
-            @click="drawerRedirect(item.value)"
-            :key="i"
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    <div class="NavWrapper">
+      <v-app-bar app absolute elevation="8">
+        <router-link class="mr-1 ml-1" :to="{ name: 'Default' }">
+          <v-img
+            src="@/assets/logo.png"
+            lazy-src="@/assets/logo.png"
+            class="mx-auto"
+            max-width="50"
+          ></v-img
+        ></router-link>
+        <v-divider class="mr-5 ml-2" vertical inset></v-divider>
+        <v-app-bar-title class="hidden-md-and-up" align-items="center">{{
+          appTitle
+        }}</v-app-bar-title>
+        <div class="hidden-sm-and-down">
+          <v-row justify="start" align-items="center">
+            <div v-if="currentUser.role.label === 'ROLE_ADMIN'">
+              <link-nav :linkTab="linkTab.admin" logout> </link-nav>
+            </div>
+            <div v-if="currentUser.role.label === 'ROLE_USER'">
+              <link-nav :linkTab="linkTab.user" logout> </link-nav>
+            </div>
+            <div v-if="!currentUser">
+              <link-nav :linkTab="linkTab.unlog"> </link-nav>
+            </div>
+          </v-row>
+        </div>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon
+          id="drawer"
+          class="hidden-md-and-up"
+          @click.stop="drawer = !drawer"
+        />
+      </v-app-bar>
+    </div>
+    <div class="DrawerWrapper">
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title> {{ appTitle }} </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list nav dense>
+          <v-list-item-group>
+            <div v-if="currentUser.role.label === 'ROLE_ADMIN'">
+              <link-drawer :linkTab="linkTab.admin" logout></link-drawer>
+            </div>
+            <div v-if="currentUser.role.label === 'ROLE_USER'">
+              <link-drawer :linkTab="linkTab.user" logout></link-drawer>
+            </div>
+            <div v-if="!currentUser">
+              <link-drawer :linkTab="linkTab.unlog"></link-drawer>
+            </div>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { mapGetters } from "vuex";
 import {
   VAppBar,
   VImg,
@@ -71,10 +78,9 @@ import {
   VListItemTitle,
   VList,
   VListItemGroup,
-  VListItemIcon,
-  VIcon,
 } from "vuetify/lib";
-import router from "@/router";
+import LinkNav from "@/components/navbar/LinkNav.vue";
+import LinkDrawer from "@/components/navbar/LinkDrawer.vue";
 
 export default Vue.extend({
   name: "NavBar",
@@ -92,38 +98,29 @@ export default Vue.extend({
     VListItemTitle,
     VList,
     VListItemGroup,
-    VListItemIcon,
-    VIcon,
+    LinkNav,
+    LinkDrawer,
   },
   data: () => {
     return {
       appTitle: "Swashing",
       drawer: false,
-      linkTab: [
-        { icon: "mdi-login-variant", text: "Connexion", value: "Login" },
-        { icon: "mdi-account-plus", text: "Inscription", value: "Register" },
-        { icon: "mdi-account-multiple", text: "Utilisateurs", value: "Users" },
-      ],
+      linkTab: {
+        admin: [],
+        user: [],
+        unlog: [
+          { icon: "mdi-login-variant", text: "Connexion", value: "Login" },
+          {
+            icon: "mdi-account-plus",
+            text: "Inscription",
+            value: "Register",
+          },
+        ],
+      },
     };
   },
-  methods: {
-    drawerRedirect(value: string) {
-      if (router.currentRoute.name != value) {
-        router.push({ name: value });
-      }
-    },
+  computed: {
+    ...mapGetters(["currentUser"]),
   },
 });
 </script>
-
-<style lang="scss">
-.NavBar {
-  .nav-link a {
-    text-decoration: none;
-  }
-  .router-link-exact-active,
-  .router-link-active {
-    color: var(--v-primary-lighten2);
-  }
-}
-</style>
