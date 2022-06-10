0072e2b6761b9b7c9c558120f3c5baec5fd5d923
KadenHD <pierraored@gmail.com>
2022-06-10 11:37:01 +0200

    Update User.ts

--- fastify-api/src/models/User.ts
+++ fastify-api/src/models/User.ts
@@ -3,24 +3,24 @@ import database from "../configs/database"
 
 const User = database.define("user", {
     id: {
-        type: Sequelize.STRING(36),
+        type: Sequelize.STRING(36), /* default length for a uuid4 string */
         defaultValue: Sequelize.UUIDV4,
         primaryKey: true
     },
     lastname: {
-        type: Sequelize.STRING(50),
+        type: Sequelize.STRING(88), /* max length for a 50 length encryption method */
         allowNull: false
     },
     firstname: {
-        type: Sequelize.STRING(50),
+        type: Sequelize.STRING(88), /* max length for a 50 length encryption method */
         allowNull: false
     },
     email: {
-        type: Sequelize.STRING(100),
+        type: Sequelize.STRING(152), /* max length for a 100 length encryption method */
         allowNull: false
     },
     password: {
-        type: Sequelize.STRING(60),
+        type: Sequelize.STRING(60), /* default length for a bcrypt hash with a 10 salt  */
         allowNull: false
     }
 })
