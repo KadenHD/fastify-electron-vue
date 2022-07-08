15bb1da5377c7d8a5d9c700432089e18cb0c8983
KadenHD <pierraored@gmail.com>
2022-07-08 17:51:47 +0200

    Update env.ts

--- fastify-api/src/utils/env.ts
+++ fastify-api/src/utils/env.ts
@@ -3,17 +3,17 @@ dotenv.config()
 
 export const env = {
     secret: {
-        iv: process.env.SECRET_IV as string,
-        crypto: process.env.SECRET_CRYPTO as string,
-        token: process.env.SECRET_TOKEN as string,
+        iv: process.env.SECRET_IV as string || '',
+        crypto: process.env.SECRET_CRYPTO as string || '',
+        token: process.env.SECRET_TOKEN as string || '',
     },
     server: {
-        port: process.env.SERVER_PORT as string,
+        port: process.env.SERVER_PORT as string || '',
     },
     db: {
-        name: process.env.DB_NAME as string,
-        user: process.env.DB_USER as string,
-        pass: process.env.DB_PASS as string,
-        host: process.env.DB_HOST as string,
+        name: process.env.DB_NAME as string || '',
+        user: process.env.DB_USER as string || '',
+        pass: process.env.DB_PASS as string || '',
+        host: process.env.DB_HOST as string || '',
     }
 }
